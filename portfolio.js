93525317b45cef5a1084f4e8cf63ff3bc583794c
nes133 <markokipi@yahoo.com>
2021-12-01 17:31:11 +0100

    changes

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,1 @@
-$(".wrapper .text-container").on('mouseover',function(){
-    $(".text-container").addClass('.image:hover + .text-container');//remove from other elements
-});
+console.log("Hello World")
