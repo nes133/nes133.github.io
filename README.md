c64640c4b694c2abc14ce79ffcba0b84e29142ec
nes133 <markokipi@yahoo.com>
2021-12-11 14:02:31 +0100

    new one

--- README.md
+++
@@ -1,1 +0,0 @@
-# nes133.github.io
---
+++ new.js
@@ -0,0 +1,37 @@
+function decrement(event) {
+  console.log(event)
+    const btn = event.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value--;
+    target.value = value;
+  }
+
+  function increment(event) {
+    const btn = event.target.parentNode.parentElement.querySelector(
+      'button[data-action="decrement"]'
+    );
+    console.log(btn)
+    const target = btn.nextElementSibling;
+    let value = Number(target.value);
+    value++;
+    target.value = value;
+  }
+
+  const decrementButtons = document.querySelectorAll(
+    `button[data-action="decrement"]`
+  );
+
+  const incrementButtons = document.querySelectorAll(
+    `button[data-action="increment"]`
+  );
+
+  decrementButtons.forEach(btn => {
+    btn.addEventListener("click", decrement);
+  });
+
+  incrementButtons.forEach(btn => {
+    btn.addEventListener("click", increment);
+  });
